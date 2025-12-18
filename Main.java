9d92451a2f846f1fc05811f20d60744737f9b39b
JOHN ALFIE RIZARRI -UCMN <johnrizarri187@gmail.com>
2025-12-18 09:37:19 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame();
+    }
+}
+
+/* Step Aside
+import java.lang.NumberFormatException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+import java.awt.Panel;
+import javax.swing.BoxLayout;
+import java.awt.Label;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JOptionPane;
+*/
